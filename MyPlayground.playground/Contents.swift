805182aeb24793b8fe7155911b56172a44793980
Hiroshi Ueda <valkyrie1973.saas@icloud.com>
2017-09-27 07:22:56 +0900

    最初のコミット

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,21 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+1 + 1
+10 - 100
+100 * 100
+1000 / 3
+100 % 3
+
+var score = 100
+score = 200
+score = 100 + 200
+score = score * 2
+let myAge = 20
+
+let taxRate = 1.08
+let answer = 300 * taxRate
+
